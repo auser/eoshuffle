b7a3a1f458b1617501a07e42a2b6e831da2864b0
Ari <ari@fullstack.io>
2018-08-14 18:33:43 -0400

    WIP

--- src/cmds/create.js
+++ src/cmds/create.js
@@ -1,13 +1,39 @@
+const yargs = require ('yargs');
+const fs = require ('fs');
+const ghdownload = require ('github-download');
+const request = require ('request');
+const {exec} = require ('shelljs');
+const tmp = require ('tmp');
+
+const checkDir = dir => {
+  return new Promise ((resolve, reject) => {
+    const content = fs.readdirSync (dir);
+    if (content.length > 0) {
+      const err =
+        `Cannot create in directory ${dir}.\n` +
+        'Something exists here already. Instead, create ' +
+        'rerun the command for a directory that does not exist';
+
+      throw new Error (err);
+    }
+  });
+};
+
+const create = async argv => {
+  const dir = argv.dir;
+  console.log ('init called for dir', dir);
+  await checkDir (dir);
+};
+
 module.exports = {
-  command: 'init [dir]',
+  command: 'init <dir>',
   desc: 'Initialize an eos project',
   alias: 'create',
-  builder: {
-    dir: {
-      default: '.',
-    },
-  },
-  handler: argv => {
-    console.log ('init called for dir', argv.dir);
-  },
+  builder: yargs =>
+    yargs.positional ('dir', {
+      describe: 'the directory to create into',
+      type: 'string',
+      default: process.cwd (),
+    }),
+  handler: create,
 };
